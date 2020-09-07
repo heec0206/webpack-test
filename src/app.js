17d880114aab939bf7dd19e2c062077151d59b8d
hcchoi <hcchoi@wbsoft.kr>
2020-09-07 10:48:46 +0900

    asdasasddass

--- src/app.js
+++ src/app.js
@@ -3,6 +3,6 @@ import './jquery-1.8.3.min.js';
 import './jquery-ui-1.8.2.js';
 import './app.css';
 
-var el = document.querySelector("#app");
-el.append = '<h1>1 + 2 = ${sum(1, 2)} </h1>';
-el.append = '<h1>webpack test</h1>';
+var el = $("#app");
+el.append( '<h1>1 + 2 = ${sum(1, 2)} </h1>');
+el.append('<h1>webpack test</h1>');
