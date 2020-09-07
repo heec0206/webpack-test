7731af0f31961e396512a8c6835b315c03eb1960
hcchoi <hcchoi@wbsoft.kr>
2020-09-07 09:59:55 +0900

    23sdf

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
 import {sum} from './math.js';
 
 window.addEventListner('DOMContentLoaded',() => {
-  sum(1, 2);
   const el = document.querySelector("#app");
   el.innerHTML = '<h1>1 - 2 = ${sum(1, 2)} </h1>';
 })
