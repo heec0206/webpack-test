c3dd1917ca3abae4852590b76a2419e083cc7ea1
hcchoi <hcchoi@wbsoft.kr>
2020-09-07 11:19:35 +0900

    sdgfxcvz

--- src/app.js
+++ src/app.js
@@ -2,7 +2,11 @@ import {sum} from './math.js';
 import './jquery-1.8.3.min.js';
 import './jquery-ui-1.8.2.js';
 import './app.css';
+import webpackImg01 from '../images/webpack_test01.png';
+import webpackImg02 from '../images/webpack_test02.png';
 
 var el = $("#app");
 el.append( '<h1>1 + 2 = ${sum(1, 2)} </h1>');
 el.append('<h1>webpack test</h1>');
+el.append('<img src= "${webpackImg01}" alt="webpack01"');
+el.append('<img src= "${webpackImg02}" alt="webpack02"');
