f4a61e9627bf600511afdb2564c5c8416fc09dd8
hcchoi <hcchoi@wbsoft.kr>
2020-09-07 11:32:02 +0900

    123123123

--- src/app.js
+++ src/app.js
@@ -5,8 +5,11 @@ import './app.css';
 import webpackImg01 from '../images/webpack_test01.png';
 import webpackImg02 from '../images/webpack_test02.png';
 
+var webpackImg011 = '../images/webpack_test01.png';
+var webpackImg022 = '../images/webpack_test02.png';
+
 var el = $("#app");
 el.append( '<h1>1 + 2 = ${sum(1, 2)} </h1>');
 el.append('<h1>webpack test</h1>');
-el.append('<img src= "${webpackImg01}" alt="webpack01"');
-el.append('<img src= "${webpackImg02}" alt="webpack02"');
+el.append('<img src=' + webpackImg011 + 'alt="webpack01"');
+el.append('zz');
