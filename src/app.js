ff33a62178c25bcd0a8f9efe2bb1922d0118e7eb
hcchoi <hcchoi@wbsoft.kr>
2020-09-07 10:28:06 +0900

    styleLoader

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 import {sum} from './math.js';
+import './app.css';
 
 var el = document.querySelector("#app");
-el.innerHTML = "<h1>1 + 2 =" + '${sum(1, 2)}' "</h1>";
+el.innerHTML = '<h1>1 + 2 = ${sum(1, 2)} </h1>';
