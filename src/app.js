93481bdec13c636f4d36748780626d3b4ac51c1d
hcchoi <hcchoi@wbsoft.kr>
2020-09-07 13:52:26 +0900

    asdasd

--- src/app.js
+++ src/app.js
@@ -2,14 +2,14 @@ import {sum} from './math.js';
 import './jquery-1.8.3.min.js';
 import './jquery-ui-1.8.2.js';
 import './app.css';
-import webpackImg01 from '../images/webpack_test01.png';
-import webpackImg02 from '../images/webpack_test02.png';
+import '../images/webpack_test01.png';
+import '../images/webpack_test02.png';
 
 //var webpackImg011 = "../images/webpack_test01.png";
 //var webpackImg022 = "../images/webpack_test02.png";
 
 var el = $("#app");
-el.append( '<h1>1 + 2 = ${sum(1, 2)} </h1>');
-el.append('<h1>webpack test</h1>');
-//el.append('<img src=' + webpackImg011 + 'alt="webpack01"');
-el.append('<img src="../images/webpack_test01.png" alt="zz"');
+el.append('<img src= "../images/webpack_test01.png"> ');
+el.append('<img src= "../images/webpack_test02.png"> ');
+//el.append( '<h1>1 + 2 = ${sum(1, 2)} </h1>');
+//el.append('<h1>webpack test</h1>');
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
     mode : 'development',
@@ -20,10 +22,28 @@ module.exports = {
           use : [{
             loader : 'file-loader',
             options : {
-              publicPath : './dist'
+              //publicPath : './dist'
+              name : '[name].[ext]?[hash]'
             }
           }]
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /(node_modules|bower_components)/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+              plugins: ['@babel/plugin-proposal-object-rest-spread']
+            }
+          }
         }
       ]
-    }
+    },
+    plugins : [
+      new HtmlWebpackPlugin({
+        template : './src/index.html',
+      }),
+      new CleanWebpackPlugin(),
+    ]
 }
